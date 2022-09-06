8b5202f756239afec6dd306b91fb1fc0d869851d
Juan Borrelli <nacho2911@hotmail.com>
2022-09-06 11:53:14 -0300

    custom user

--- django_base/settings.py
+++ django_base/settings.py
@@ -176,6 +176,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'users.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- users/models.py
+++ users/models.py
@@ -1,8 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+class User(AbstractUser):
+    # add additional fields in here
+    pass
+
+    def __str__(self):
+        return self.username
+        
 class User_profile(models.Model):
     CHOICES = (
         ('Masculino','Masculino'),
