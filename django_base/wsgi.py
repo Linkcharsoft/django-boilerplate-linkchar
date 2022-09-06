3c362852fa1f3063c748ec1375419907dea59a0c
Juan Borrelli <nacho2911@hotmail.com>
2022-09-06 12:09:03 -0300

    import error

--- django_base/wsgi.py
+++ django_base/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
-from settings import IS_SERVER
+from django_base.settings import IS_SERVER
 import os
 from django.core.wsgi import get_wsgi_application
 
