79af50d5caffedf976c360abe2ff555e2ff9bb88
Juan Borrelli <nacho2911@hotmail.com>
2023-03-07 14:24:27 -0300

    utils with 'get_date_with_timezone'

--- django_base/utils.py
+++ django_base/utils.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
 from datetime import datetime
 
-def get_today():
-    return timezone.make_aware(datetime.now(), timezone.get_default_timezone())
+def get_date_with_timezone(date):
+    return timezone.make_aware(date, timezone.get_default_timezone())
